6daa96f6326e965a54702d4cfd952de352e5273a
Jorge R. <jorge.shortys@gmail.com>
2017-10-15 21:58:17 +0200

    New Methods.

---
+++ src/testW10/Functions.java
@@ -0,0 +1,30 @@
+package testW10;
+
+public class Functions {
+	
+	private String Description = "";
+	private boolean Conection;
+	
+	
+	public Functions() {
+		
+		
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	public boolean isConection() {
+		return Conection;
+	}
+
+	public void setConection(boolean conection) {
+		Conection = conection;
+	}
+
+}
