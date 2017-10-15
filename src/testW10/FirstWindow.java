53977714cb62e4d1f3403abca35971144d0a02b2
Jorge R. <jorge.shortys@gmail.com>
2017-10-15 19:13:53 +0200

    test

---
+++ src/testW10/FirstWindow.java
@@ -0,0 +1,43 @@
+package testW10;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class FirstWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FirstWindow window = new FirstWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FirstWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
