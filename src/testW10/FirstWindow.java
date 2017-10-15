65c86712dd68e4bf2af9c568346c547063f2a18e
Jorge R. <jorge.shortys@gmail.com>
2017-10-15 19:31:18 +0200

    New Window

--- src/testW10/FirstWindow.java
+++ src/testW10/FirstWindow.java
@@ -3,10 +3,15 @@ package testW10;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class FirstWindow {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -36,9 +41,19 @@ public class FirstWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 455, 225);
+		frame.setName("testW10");
+		frame.setBounds(100, 100, 477, 225);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(121, 11, 330, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("IP/MACHINE NAME: ");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 9));
+		lblNewLabel.setBounds(10, 14, 113, 14);
+		frame.getContentPane().add(lblNewLabel);
 	}
-
 }
