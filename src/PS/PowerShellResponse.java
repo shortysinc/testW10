84c558b9f49c190691306ab8f65db3e0fe24768c
Jorge R. <jorge.shortys@gmail.com>
2017-10-22 17:19:32 +0200

    test

--- src/PS/PowerShellResponse.java
+++ src/PS/PowerShellResponse.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package testW10;
+package PS;
 
 /**
  * Response of PowerShell command. This object encapsulate all the useful 
